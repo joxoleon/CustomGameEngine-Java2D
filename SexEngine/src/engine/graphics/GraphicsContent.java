b52170b6ad85fe0e8d556f3b55ca6a123b43955f
Jovan Radivojsa <jovan.radivojsa@gmail.com>
2015-04-30 00:00:36 +0200

    glupost

--- SexEngine/src/engine/graphics/GraphicsContent.java
+++ SexEngine/src/engine/graphics/GraphicsContent.java
@@ -140,6 +140,44 @@ public class GraphicsContent
 	}
 	
 	public void
+	loadTerrainTiles(String path, int width, int height, int rows, int columns, int count)
+	{
+		try
+		{
+			BufferedImage image = ImageIO.read(new File(path));
+			BufferedImage[] sprites = new BufferedImage[count];
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < columns; j++)
+				{
+					if(i * columns + j > count)
+						break;
+					
+					BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+					Graphics2D g2d = (Graphics2D)newImage.getGraphics();
+					g2d.drawImage(
+							image, 
+							0, 0, width, height, 
+							j * width, i * height, (j + 1) * width, (i + 1) * height,
+							null);
+					
+					//Dodaj ucitavanje imena iz fajla
+					//Dodaj sprajt sa odgovarajucim imenom u images
+					
+				}
+			}
+			
+			
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.err.println("File not found! Path: " + path);
+		}
+	    
+	}
+	
+	public void
 	loadSpriteSheet(String name, String path, int xorigin, int yorigin, int width, int height, int rows, int columns, int count)
 	{
 		try 
@@ -172,6 +210,8 @@ public class GraphicsContent
 			System.err.println("File not found! Path: " + path);
 		}
 	}
+	
+
 	
 	public void
 	unloadImage(String name)
